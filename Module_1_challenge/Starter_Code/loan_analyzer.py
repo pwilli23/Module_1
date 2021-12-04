561d588fa13b3c2518c19867c46c5c47b5bb9f07
pwilli23 <93749067+pwilli23@users.noreply.github.com>
2021-12-03 22:27:01 -0500

    Add files via upload
    Parts 1,2,3 finished

--- Module_1_challenge/Starter_Code/loan_analyzer.py
+++ Module_1_challenge/Starter_Code/loan_analyzer.py
@@ -74,7 +74,13 @@ loan = {
 
 # @TODO: Use get() on the dictionary of additional information to extract the Future Value and Remaining Months on the loan.
 # Print each variable.
-# YOUR CODE HERE!
+future_value = loan["future_value"]
+print(f"The future value amount is: ${future_value}")
+
+remaining_months = loan["remaining_months"]
+print(f"Total remaining months is {remaining_months} ")
+
+loan_price = loan["loan_price"]
 
 
 # @TODO: Use the formula for Present Value to calculate a "fair value" of the loan.
@@ -82,13 +88,18 @@ loan = {
 #   You'll want to use the **monthly** version of the present value formula.
 #   HINT: Present Value = Future Value / (1 + Discount_Rate/12) ** remaining_months
 
-# YOUR CODE HERE!
+present_value = future_value/ (1+0.2/12)** remaining_months
+
+print(present_value)
 
 # If Present Value represents what the loan is really worth, does it make sense to buy the loan at its cost?
 # @TODO: Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
 #    If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
 #    Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
-# YOUR CODE HERE!
+if present_value >= loan_price:
+    print("This loan is at least worth the cost of buying it")
+else:
+    print("This loan is not worth the cost of buying it")
 
 
 """Part 3: Perform Financial Calculations.
@@ -113,7 +124,7 @@ new_loan = {
 # @TODO: Define a new function that will be used to calculate present value.
 #    This function should include parameters for `future_value`, `remaining_months`, and the `annual_discount_rate`
 #    The function should return the `present_value` for the loan.
-# YOUR CODE HERE!
+#YOUR CODE HERE!
 
 
 # @TODO: Use the function to calculate the present value of the new loan given below.
